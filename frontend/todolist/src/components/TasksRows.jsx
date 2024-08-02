a729af051aebbdc2453c3b270ff4b4f78a9405b6
Bahiy <bahiy14@gmail.com>
2024-08-02 02:08:21 -0300

    Re-styled to responsive content

--- frontend/todolist/src/components/TasksRows.jsx
+++ frontend/todolist/src/components/TasksRows.jsx
@@ -9,13 +9,13 @@ const TasksRows = () => {
   const [editTaskTitle, setEditTaskTitle] = useState("");
   const [editTaskStatus, setEditTaskStatus] = useState("");
 
-  const style = "px-4 py-2 mborder-solid border text-center";
+  const reverTask = tasks.reverse();
+  const style = "px-4 py-2 border-solid border text-center";
 
   const getTasks = async () => {
     try {
       const data = (await api.get()).data;
       console.log(data);
-      
       setTasks(data);
     } catch (error) {
       console.log("Error on fetching tasks:", error);
@@ -78,99 +78,104 @@ const TasksRows = () => {
       window.speechSynthesis.speak(ut);
     }
   };
+
   useEffect(() => {
     getTasks();
   }, []);
 
   return (
-    <div>
+    <div className="p-4">
       <FormTable getTasks={getTasks} />
-      <table className="border-collapse w-full">
-        <thead>
-          <tr>
-            <th className={style}>Tarefa</th>
-            <th className={style}>Criada em</th>
-            <th className={style}>Status</th>
-            <th className={style}>Ações</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {tasks.map((task) => (
-            <tr key={task.id}>
-              <td className="px-4 py-5 margin border text-center flex justify-between items-center">
-                {editTaskId === task.id ? (
-                  <input
-                    type="text"
-                    value={editTaskTitle}
-                    onChange={(e) => setEditTaskTitle(e.target.value)}
-                    className="w-full p-2 border-solid border rounded-md"
-                  />
-                ) : (
-                  task.title
-                )}
-                <button
-                  id="speech"
-                  onClick={() => {
-                    handleVoiceClick(task.title);
-                  }}
-                  className="material-symbols-outlined text-base"
-                >
-                  volume_up
-                </button>
-              </td>
-              <td className={style}>{task.created_at}</td>
-              <td className={style}>
-                {editTaskId === task.id ? (
-                  <select
-                    className="hover:bg-neutral-300 w-full p-3 text-md capitalize rounded-xl bg-neutral-200"
-                    value={editTaskStatus}
-                    onChange={(e) => setEditTaskStatus(e.target.value)}
-                  >
-                    <option value="pendente">Pendente</option>
-                    <option value="em andamento">Em andamento</option>
-                    <option value="finalizada">Finalizada</option>
-                  </select>
-                ) : (
-                  <select
-                    className="hover:bg-neutral-300 w-full p-3 text-md capitalize rounded-xl bg-neutral-200"
-                    value={task.status}
-                    onChange={(e) => putTask(task.id, e.target.value)}
-                    disabled
-                  >
-                    <option value="pendente">Pendente</option>
-                    <option value="em andamento">Em andamento</option>
-                    <option value="finalizada">Finalizada</option>
-                  </select>
-                )}
-              </td>
-              <td className={style}>
-                {editTaskId === task.id ? (
-                  <button
-                    onClick={() => handleSaveClick(task.id)}
-                    className="rounded-md p-2 text-neutral-100 inline-flex items-center justify-center cursor-pointer m-1 bg-green-400"
-                  >
-                    <span className="material-symbols-outlined">save</span>
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => handleEditClick(task)}
-                    className="rounded-md p-2 text-neutral-100 inline-flex items-center justify-center cursor-pointer m-1 bg-orange-400"
-                  >
-                    <span className="material-symbols-outlined">edit</span>
-                  </button>
-                )}
-                <button
-                  onClick={() => deleteTask(task.id)}
-                  className="rounded-md p-2 text-neutral-100 inline-flex items-center justify-center cursor-pointer m-1 bg-red-600"
-                >
-                  <span className="material-symbols-outlined">delete</span>
-                </button>
-              </td>
+      <div className="overflow-x-auto">
+        <table className="border-collapse w-full">
+          <thead>
+            <tr>
+              <th className={`${style} text-sm md:text-base`}>Tarefa</th>
+              <th className={`${style} text-sm md:text-base`}>Criada em</th>
+              <th className={`${style} text-sm md:text-base`}>Status</th>
+              <th className={`${style} text-sm md:text-base`}>Ações</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {reverTask.map((task) => (
+              <tr key={task.id}>
+                <td className="px-2 py-3 md:px-4 md:py-5 border text-center flex justify-between items-center text-sm md:text-base">
+                  {editTaskId === task.id ? (
+                    <input
+                      type="text"
+                      value={editTaskTitle}
+                      onChange={(e) => setEditTaskTitle(e.target.value)}
+                      className="w-full p-2 border rounded-md"
+                    />
+                  ) : (
+                    task.title
+                  )}
+                  <button
+                    id="speech"
+                    onClick={() => {
+                      handleVoiceClick(task.title);
+                    }}
+                    className="material-symbols-outlined text-sm md:text-base"
+                  >
+                    volume_up
+                  </button>
+                </td>
+                <td className={`${style} text-sm md:text-base`}>
+                  {task.created_at}
+                </td>
+                <td className={`${style} text-sm md:text-base`}>
+                  {editTaskId === task.id ? (
+                    <select
+                      className="hover:bg-neutral-300 w-full p-2 md:p-3 text-sm md:text-md capitalize rounded-xl bg-neutral-200"
+                      value={editTaskStatus}
+                      onChange={(e) => setEditTaskStatus(e.target.value)}
+                    >
+                      <option value="pendente">Pendente</option>
+                      <option value="em andamento">Em andamento</option>
+                      <option value="finalizada">Finalizada</option>
+                    </select>
+                  ) : (
+                    <select
+                      className="hover:bg-neutral-300 w-full p-2 md:p-3 text-sm md:text-md capitalize rounded-xl bg-neutral-200"
+                      value={task.status}
+                      onChange={(e) => putTask(task.id, e.target.value)}
+                      disabled
+                    >
+                      <option value="pendente">Pendente</option>
+                      <option value="em andamento">Em andamento</option>
+                      <option value="finalizada">Finalizada</option>
+                    </select>
+                  )}
+                </td>
+                <td className={`${style} text-sm md:text-base`}>
+                  {editTaskId === task.id ? (
+                    <button
+                      onClick={() => handleSaveClick(task.id)}
+                      className="rounded-md p-2 text-neutral-100 inline-flex items-center justify-center cursor-pointer m-1 bg-green-400 text-xs md:text-sm"
+                    >
+                      <span className="material-symbols-outlined">save</span>
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleEditClick(task)}
+                      className="rounded-md p-2 text-neutral-100 inline-flex items-center justify-center cursor-pointer m-1 bg-orange-400 text-xs md:text-sm"
+                    >
+                      <span className="material-symbols-outlined">edit</span>
+                    </button>
+                  )}
+                  <button
+                    onClick={() => deleteTask(task.id)}
+                    className="rounded-md p-2 text-neutral-100 inline-flex items-center justify-center cursor-pointer m-1 bg-red-600 text-xs md:text-sm"
+                  >
+                    <span className="material-symbols-outlined">delete</span>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
